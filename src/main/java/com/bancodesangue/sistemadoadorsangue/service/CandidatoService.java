0b27bbae653e3e780461408335eefccc2999fe5c
guibsonkrause <oliveira.guibson@hotmail.com>
2023-11-12 16:40:24 -0300

    adicionado metodos basicos ao service

--- src/main/java/com/bancodesangue/sistemadoadorsangue/service/CandidatoService.java
+++ src/main/java/com/bancodesangue/sistemadoadorsangue/service/CandidatoService.java
@@ -5,7 +5,9 @@ import com.bancodesangue.sistemadoadorsangue.repository.CandidatoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CandidatoService {
@@ -21,5 +23,22 @@ public class CandidatoService {
         return candidatoRepository.findAll();
     }
 
-    // Outros métodos de serviço ...
+    public List<Candidato> findByEstado(String estado) {
+        return candidatoRepository.findByEstado(estado);
+    }
+
+    public Double findAverageIMCByAgeRange(Date startDate, Date endDate) {
+        return candidatoRepository.findAverageIMCByAgeRange(startDate, endDate);
+    }
+
+    public Long countObesosBySexo(String sexo) {
+        return candidatoRepository.countObesosBySexo(sexo);
+    }
+
+    public Long countByTipoSanguineo(String tipoSanguineo) {
+        return candidatoRepository.countByTipoSanguineo(tipoSanguineo);
+    }
+
+    // Você pode adicionar outros métodos de serviço que façam sentido para a lógica do seu negócio
+    // Por exemplo, métodos para calcular a porcentagem de candidatos obesos, a quantidade de possíveis doadores por tipo sanguíneo, etc.
 }
