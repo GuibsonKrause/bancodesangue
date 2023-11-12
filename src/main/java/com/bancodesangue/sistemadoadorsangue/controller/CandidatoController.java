a33c6d8969c626be1c00ea8bf8145218d6fd64ce
guibsonkrause <oliveira.guibson@hotmail.com>
2023-11-12 16:41:11 -0300

    implementado CRUD controller

--- src/main/java/com/bancodesangue/sistemadoadorsangue/controller/CandidatoController.java
+++ src/main/java/com/bancodesangue/sistemadoadorsangue/controller/CandidatoController.java
@@ -3,6 +3,7 @@ package com.bancodesangue.sistemadoadorsangue.controller;
 import com.bancodesangue.sistemadoadorsangue.model.Candidato;
 import com.bancodesangue.sistemadoadorsangue.service.CandidatoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,11 +20,44 @@ public class CandidatoController {
         this.candidatoService = candidatoService;
     }
 
+    // Já implementado - listar todos os candidatos
     @GetMapping
     public ResponseEntity<List<Candidato>> getAllCandidatos() {
         List<Candidato> candidatos = candidatoService.findAll();
         return ResponseEntity.ok(candidatos);
     }
 
-    // Outros endpoints ...
+    // Adicionar um novo candidato
+    @PostMapping
+    public ResponseEntity<Candidato> addCandidato(@RequestBody Candidato candidato) {
+        Candidato novoCandidato = candidatoService.save(candidato);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoCandidato);
+    }
+
+    // Buscar candidato pelo ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Candidato> getCandidatoById(@PathVariable Long id) {
+        Candidato candidato = candidatoService.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Candidato não encontrado"));
+        return ResponseEntity.ok(candidato);
+    }
+
+    // Atualizar um candidato existente
+    @PutMapping("/{id}")
+    public ResponseEntity<Candidato> updateCandidato(@PathVariable Long id, @RequestBody Candidato candidatoDetails) {
+        Candidato updatedCandidato = candidatoService.update(id, candidatoDetails);
+        return ResponseEntity.ok(updatedCandidato);
+    }
+
+    // Deletar um candidato
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCandidato(@PathVariable Long id) {
+        candidatoService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    // Outros métodos conforme necessário...
+
+    // A lógica destes métodos deve ser implementada no CandidatoService.
+    // Lembre-se de tratar exceções apropriadamente, possivelmente usando um @ControllerAdvice para manipulação global de erros.
 }
