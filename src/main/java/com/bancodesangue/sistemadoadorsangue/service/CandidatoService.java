508ab87d54be0058c28ba1d6b4e122e00dfe20c2
guibsonkrause <oliveira.guibson@hotmail.com>
2023-11-12 16:49:58 -0300

    implementado CRUD service

--- src/main/java/com/bancodesangue/sistemadoadorsangue/service/CandidatoService.java
+++ src/main/java/com/bancodesangue/sistemadoadorsangue/service/CandidatoService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 @Service
 public class CandidatoService {
@@ -37,6 +37,48 @@ public class CandidatoService {
 
     public Long countByTipoSanguineo(String tipoSanguineo) {
         return candidatoRepository.countByTipoSanguineo(tipoSanguineo);
+    }
+
+    public Optional<Candidato> findById(Long id) {
+        return candidatoRepository.findById(id);
+    }
+
+    public Candidato save(Candidato candidato) {
+        return candidatoRepository.save(candidato);
+    }
+
+    public Candidato update(Long id, Candidato candidatoDetails) {
+        Candidato candidato = candidatoRepository.findById(id)
+            .orElseThrow(() -> new IllegalStateException("Candidato com ID " + id + " não existe."));
+        
+        candidato.setNome(candidatoDetails.getNome());
+        candidato.setCpf(candidatoDetails.getCpf());
+        candidato.setRg(candidatoDetails.getRg());
+        candidato.setDataNasc(candidatoDetails.getDataNasc());
+        candidato.setSexo(candidatoDetails.getSexo());
+        candidato.setMae(candidatoDetails.getMae());
+        candidato.setPai(candidatoDetails.getPai());
+        candidato.setEmail(candidatoDetails.getEmail());
+        candidato.setCep(candidatoDetails.getCep());
+        candidato.setEndereco(candidatoDetails.getEndereco());
+        candidato.setNumero(candidatoDetails.getNumero());
+        candidato.setBairro(candidatoDetails.getBairro());
+        candidato.setCidade(candidatoDetails.getCidade());
+        candidato.setEstado(candidatoDetails.getEstado());
+        candidato.setTelefoneFixo(candidatoDetails.getTelefoneFixo());
+        candidato.setCelular(candidatoDetails.getCelular());
+        candidato.setAltura(candidatoDetails.getAltura());
+        candidato.setPeso(candidatoDetails.getPeso());
+        candidato.setTipoSanguineo(candidatoDetails.getTipoSanguineo());
+
+        return candidatoRepository.save(candidato);
+    }
+
+
+    public void delete(Long id) {
+        Candidato candidato = candidatoRepository.findById(id)
+            .orElseThrow(() -> new IllegalStateException("Candidato com ID " + id + " não existe."));
+        candidatoRepository.delete(candidato);
     }
 
     // Você pode adicionar outros métodos de serviço que façam sentido para a lógica do seu negócio
