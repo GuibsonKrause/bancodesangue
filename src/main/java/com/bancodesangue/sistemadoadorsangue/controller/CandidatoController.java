75cd1dabbe9def2dc7bb8474580aead1f718e0e4
guibsonkrause <oliveira.guibson@hotmail.com>
2023-11-16 17:10:56 -0300

    Implementado extractTextFromPdf

--- src/main/java/com/bancodesangue/sistemadoadorsangue/controller/CandidatoController.java
+++ src/main/java/com/bancodesangue/sistemadoadorsangue/controller/CandidatoController.java
@@ -3,12 +3,18 @@ package com.bancodesangue.sistemadoadorsangue.controller;
 import com.bancodesangue.sistemadoadorsangue.dto.ResultadoDTO;
 import com.bancodesangue.sistemadoadorsangue.model.Candidato;
 import com.bancodesangue.sistemadoadorsangue.service.CandidatoService;
+import com.bancodesangue.sistemadoadorsangue.service.PDFService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -16,9 +22,13 @@ import java.util.List;
 public class CandidatoController {
 
     private final CandidatoService candidatoService;
+    private final PDFService pdfService;
+    private final ObjectMapper objectMapper; // Usado para conversão de JSON
 
-    public CandidatoController(CandidatoService candidatoService) {
+    public CandidatoController(CandidatoService candidatoService, PDFService pdfService, ObjectMapper objectMapper) {
         this.candidatoService = candidatoService;
+        this.pdfService = pdfService;
+        this.objectMapper = objectMapper;
     }
 
     @GetMapping
@@ -34,18 +44,29 @@ public class CandidatoController {
     }
 
     @PostMapping("/import")
-    public ResponseEntity<Object> importCandidatos(@RequestBody List<Candidato> candidatos) {
-        try {
-            candidatoService.saveAll(candidatos);
+    public ResponseEntity<Object> importCandidatos(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty() || file.getContentType() == null) {
+            return ResponseEntity.badRequest().body("Arquivo inválido ou não é um PDF.");
+        }
 
+        if (!file.getContentType().equals("application/pdf")) {
+            return ResponseEntity.badRequest().body("O arquivo não é um PDF.");
+        }
+
+        try {
+            String jsonContent = pdfService.extractTextFromPdf(file);
+            String sanitizedJson = jsonContent.replaceAll("\\r\\n|\\r|\\n", " ");
+            List<Candidato> candidatos = objectMapper.readValue(sanitizedJson, new TypeReference<List<Candidato>>() {});
+
+            candidatoService.saveAll(candidatos);
             ResultadoDTO resultado = candidatoService.processarCandidatos(candidatos);
 
             return new ResponseEntity<>(resultado, HttpStatus.CREATED);
         } catch (DataIntegrityViolationException e) {
-            // Lógica para tratar violações de integridade, como entradas duplicadas
             return ResponseEntity.badRequest().body("Falha na validação dos dados: " + e.getMessage());
+        } catch (IOException e) {
+            return ResponseEntity.badRequest().body("Erro na leitura do arquivo PDF: " + e.getMessage());
         } catch (Exception e) {
-            // Lógica para tratar outras exceções
             return ResponseEntity.internalServerError().body("Erro no servidor: " + e.getMessage());
         }
     }
